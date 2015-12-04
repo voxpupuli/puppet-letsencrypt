4209128eeea6c159ff3131d4b2b28a56f8cd4d70
Garrett Honeycutt <code@garretthoneycutt.com>
2015-12-03 20:12:39 -0500

    Change ruby code to conform with rubocop

--- spec/defines/letsencrypt_certonly_spec.rb
+++ spec/defines/letsencrypt_certonly_spec.rb
@@ -1,5 +1,5 @@
 describe 'letsencrypt::certonly' do
-  let(:facts) {{ osfamily: 'Debian' }}
+  let(:facts) { { osfamily: 'Debian' } }
   let(:pre_condition) { 'include letsencrypt' }
 
   context 'with a single domain' do
@@ -11,31 +11,31 @@ describe 'letsencrypt::certonly' do
 
   context 'with multiple domains' do
     let(:title) { 'foo' }
-    let(:params) {{ domains: ['foo.example.com', 'bar.example.com']}}
+    let(:params) { { domains: ['foo.example.com', 'bar.example.com'] } }
     it { is_expected.to contain_exec('letsencrypt certonly foo').with_command '/opt/letsencrypt/letsencrypt-auto certonly --standalone -d foo.example.com -d bar.example.com' }
   end
 
   context 'with custom path' do
     let(:title) { 'foo.example.com' }
-    let(:params) {{ letsencrypt_path: '/usr/lib/letsencrypt' }}
+    let(:params) { { letsencrypt_path: '/usr/lib/letsencrypt' } }
     it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command '/usr/lib/letsencrypt/letsencrypt-auto certonly --standalone -d foo.example.com' }
   end
 
   context 'with custom plugin' do
     let(:title) { 'foo.example.com' }
-    let(:params) {{ plugin: 'apache' }}
+    let(:params) { { plugin: 'apache' } }
     it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command '/opt/letsencrypt/letsencrypt-auto certonly --apache -d foo.example.com' }
   end
 
   context 'with invalid plugin' do
     let(:title) { 'foo.example.com' }
-    let(:params) {{ plugin: 'bad' }}
+    let(:params) { { plugin: 'bad' } }
     it { is_expected.to raise_error Puppet::Error }
   end
 
   context 'when specifying additional arguments' do
     let(:title) { 'foo.example.com' }
-    let(:params) {{ additional_args: ['--foo bar', '--baz quux'] }}
+    let(:params) { { additional_args: ['--foo bar', '--baz quux'] } }
     it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command '/opt/letsencrypt/letsencrypt-auto certonly --standalone -d foo.example.com --foo bar --baz quux' }
   end
 end
