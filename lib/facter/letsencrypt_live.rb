817bb64f243ea83c3839cdd3411ace3e731c4afc
Cliff Wells <cwells@focusvision.com>
2017-12-08 23:39:53 -0800

    style fixes

--- lib/facter/letsencrypt_live.rb
+++ lib/facter/letsencrypt_live.rb
@@ -7,7 +7,7 @@ Facter.add(:letsencrypt_live) do
     if !livedir.directory?
       []
     else
-      livedir.children.select(&:directory?).map(&:basename).map { |name| name.to_s }
+      livedir.children.select(&:directory?).map(&:basename).map(&:to_s)
     end
   end
 end
