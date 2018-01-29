2cc1b9a84d5f1abccc34e88e45c255ca6e6035b5
Michael Englehorn <michael@englehorn.com>
2018-01-29 15:39:58 -0600

    Fixes #81 added -n flag to certbot to run in unattended mode.

--- manifests/certonly.pp
+++ manifests/certonly.pp
@@ -53,9 +53,9 @@ define letsencrypt::certonly (
   }
 
   if ($custom_plugin) {
-    $command_start = "${letsencrypt_command} --text --agree-tos certonly "
+    $command_start = "${letsencrypt_command} --text --agree-tos -n certonly "
   } else {
-    $command_start = "${letsencrypt_command} --text --agree-tos certonly -a ${plugin} "
+    $command_start = "${letsencrypt_command} --text --agree-tos -n certonly -a ${plugin} "
   }
 
   $command_domains = $plugin ? {
--- spec/defines/letsencrypt_certonly_spec.rb
+++ spec/defines/letsencrypt_certonly_spec.rb
@@ -11,21 +11,21 @@ describe 'letsencrypt::certonly' do
 
         it { is_expected.to contain_exec('letsencrypt certonly foo.example.com') }
         it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_creates '/etc/letsencrypt/live/foo.example.com/cert.pem' }
-        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos certonly -a standalone -d foo.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos -n certonly -a standalone -d foo.example.com' }
       end
 
       context 'with multiple domains' do
         let(:title) { 'foo' }
         let(:params) { { domains: ['foo.example.com', 'bar.example.com'] } }
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo').with_command 'letsencrypt --text --agree-tos certonly -a standalone -d foo.example.com -d bar.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo').with_command 'letsencrypt --text --agree-tos -n certonly -a standalone -d foo.example.com -d bar.example.com' }
       end
 
       context 'with custom command' do
         let(:title) { 'foo.example.com' }
         let(:params) { { letsencrypt_command: '/usr/lib/letsencrypt/letsencrypt-auto' } }
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command '/usr/lib/letsencrypt/letsencrypt-auto --text --agree-tos certonly -a standalone -d foo.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command '/usr/lib/letsencrypt/letsencrypt-auto --text --agree-tos -n certonly -a standalone -d foo.example.com' }
       end
 
       context 'with webroot plugin' do
@@ -35,7 +35,7 @@ describe 'letsencrypt::certonly' do
             webroot_paths: ['/var/www/foo'] }
         end
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos certonly -a webroot --webroot-path /var/www/foo -d foo.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos -n certonly -a webroot --webroot-path /var/www/foo -d foo.example.com' }
       end
 
       context 'with webroot plugin and multiple domains' do
@@ -46,7 +46,7 @@ describe 'letsencrypt::certonly' do
             webroot_paths: ['/var/www/foo', '/var/www/bar'] }
         end
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo').with_command 'letsencrypt --text --agree-tos certonly -a webroot --webroot-path /var/www/foo -d foo.example.com --webroot-path /var/www/bar -d bar.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo').with_command 'letsencrypt --text --agree-tos -n certonly -a webroot --webroot-path /var/www/foo -d foo.example.com --webroot-path /var/www/bar -d bar.example.com' }
       end
 
       context 'with webroot plugin, one webroot, and multiple domains' do
@@ -57,7 +57,7 @@ describe 'letsencrypt::certonly' do
             webroot_paths: ['/var/www/foo'] }
         end
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo').with_command 'letsencrypt --text --agree-tos certonly -a webroot --webroot-path /var/www/foo -d foo.example.com -d bar.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo').with_command 'letsencrypt --text --agree-tos -n certonly -a webroot --webroot-path /var/www/foo -d foo.example.com -d bar.example.com' }
       end
 
       context 'with webroot plugin and no webroot_paths' do
@@ -71,7 +71,7 @@ describe 'letsencrypt::certonly' do
         let(:title) { 'foo.example.com' }
         let(:params) { { plugin: 'apache' } }
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos certonly -a apache -d foo.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos -n certonly -a apache -d foo.example.com' }
       end
 
       context 'with custom plugin and manage cron' do
@@ -84,7 +84,7 @@ describe 'letsencrypt::certonly' do
         end
 
         it { is_expected.to contain_cron('letsencrypt renew cron foo.example.com').with_command '/var/lib/puppet/letsencrypt/renew-foo.example.com.sh' }
-        it { is_expected.to contain_file('/var/lib/puppet/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\nletsencrypt --text --agree-tos certonly -a apache --keep-until-expiring -d foo.example.com" }
+        it { is_expected.to contain_file('/var/lib/puppet/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\nletsencrypt --text --agree-tos -n certonly -a apache --keep-until-expiring -d foo.example.com" }
       end
 
       context 'with custom puppet_vardir path and manage_cron' do
@@ -96,7 +96,7 @@ describe 'letsencrypt::certonly' do
         end
 
         it { is_expected.to contain_cron('letsencrypt renew cron foo.example.com').with_command '/tmp/custom_vardir/letsencrypt/renew-foo.example.com.sh' }
-        it { is_expected.to contain_file('/tmp/custom_vardir/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\nletsencrypt --text --agree-tos certonly -a apache --keep-until-expiring -d foo.example.com" }
+        it { is_expected.to contain_file('/tmp/custom_vardir/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\nletsencrypt --text --agree-tos -n certonly -a apache --keep-until-expiring -d foo.example.com" }
       end
 
       context 'with custom plugin and manage cron and cron_success_command' do
@@ -111,14 +111,14 @@ describe 'letsencrypt::certonly' do
         end
 
         it { is_expected.to contain_cron('letsencrypt renew cron foo.example.com').with_command '/var/lib/puppet/letsencrypt/renew-foo.example.com.sh' }
-        it { is_expected.to contain_file('/var/lib/puppet/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\n(echo before) && letsencrypt --text --agree-tos certonly -a apache --keep-until-expiring -d foo.example.com && (echo success)" }
+        it { is_expected.to contain_file('/var/lib/puppet/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\n(echo before) && letsencrypt --text --agree-tos -n certonly -a apache --keep-until-expiring -d foo.example.com && (echo success)" }
       end
 
       context 'without plugin' do
         let(:title) { 'foo.example.com' }
         let(:params) { { custom_plugin: true } }
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos certonly -d foo.example.com' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos -n certonly -d foo.example.com' }
       end
 
       context 'with invalid plugin' do
@@ -132,7 +132,7 @@ describe 'letsencrypt::certonly' do
         let(:title) { 'foo.example.com' }
         let(:params) { { additional_args: ['--foo bar', '--baz quux'] } }
 
-        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos certonly -a standalone -d foo.example.com --foo bar --baz quux' }
+        it { is_expected.to contain_exec('letsencrypt certonly foo.example.com').with_command 'letsencrypt --text --agree-tos -n certonly -a standalone -d foo.example.com --foo bar --baz quux' }
       end
 
       describe 'when specifying custom environment variables' do
@@ -157,7 +157,7 @@ describe 'letsencrypt::certonly' do
         end
 
         it { is_expected.to contain_cron('letsencrypt renew cron foo.example.com').with_command '/var/lib/puppet/letsencrypt/renew-foo.example.com.sh' }
-        it { is_expected.to contain_file('/var/lib/puppet/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\nletsencrypt --text --agree-tos certonly -a standalone --keep-until-expiring -d foo.example.com > /dev/null 2>&1" }
+        it { is_expected.to contain_file('/var/lib/puppet/letsencrypt/renew-foo.example.com.sh').with_content "#!/bin/sh\nletsencrypt --text --agree-tos -n certonly -a standalone --keep-until-expiring -d foo.example.com > /dev/null 2>&1" }
       end
     end
   end
