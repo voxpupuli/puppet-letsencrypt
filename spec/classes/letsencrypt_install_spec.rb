141a2dd862fbf4daa48c7ad676e0503c0e81a2d3
Fabien COMBERNOUS <fabien.combernous@adullact.org>
2019-01-12 00:44:56 +0100

    migrate tests to rspec-puppet-facts

--- spec/classes/letsencrypt_install_spec.rb
+++ spec/classes/letsencrypt_install_spec.rb
@@ -1,92 +1,101 @@
 require 'spec_helper'
 
 describe 'letsencrypt::install' do
-  let(:params) { default_params.merge(additional_params) }
-  let(:default_params) do
-    {
-      configure_epel: false,
-      package_ensure: 'installed',
-      manage_install: true,
-      manage_dependencies: true,
-      path: '/opt/letsencrypt',
-      repo: 'https://github.com/certbot/certbot.git',
-      version: 'v0.9.3',
-      package_name: 'letsencrypt'
-    }
-  end
-  let(:additional_params) { {} }
-
-  describe 'with install_method => package' do
-    let(:additional_params) { { install_method: 'package' } }
-
-    it { is_expected.to compile }
-
-    it 'contains the correct resources' do
-      is_expected.not_to contain_vcsrepo('/opt/letsencrypt')
-      is_expected.not_to contain_package('python')
-      is_expected.not_to contain_package('git')
-
-      is_expected.to contain_package('letsencrypt').with_ensure('installed')
+  on_supported_os.each do |os, facts|
+    let :facts do
+      facts
     end
+    let(:params) { default_params.merge(additional_params) }
+    let(:default_params) do
+      {
+        configure_epel: false,
+        package_ensure: 'installed',
+        manage_install: true,
+        manage_dependencies: true,
+        path: '/opt/letsencrypt',
+        repo: 'https://github.com/certbot/certbot.git',
+        version: 'v0.9.3',
+        package_name: 'letsencrypt'
+      }
+    end
+    let(:additional_params) { {} }
 
-    describe 'with configure_epel => true' do
-      let(:facts) { { osfamily: 'RedHat', operatingsystem: 'RedHat', operatingsystemrelease: '7.0.1406', operatingsystemmajrelease: '7' } }
-      let(:additional_params) { { install_method: 'package', configure_epel: true } }
+    context "on #{os} based operating systems" do
+      describe 'with install_method => package' do
+        let(:additional_params) { { install_method: 'package' } }
 
-      it { is_expected.to compile }
+        it { is_expected.to compile }
 
-      it 'contains the correct resources' do
-        is_expected.to contain_class('epel')
-        is_expected.to contain_package('letsencrypt').that_requires('Class[epel]')
+        it 'contains the correct resources' do
+          is_expected.not_to contain_vcsrepo('/opt/letsencrypt')
+          is_expected.not_to contain_package('python')
+          is_expected.not_to contain_package('git')
+
+          is_expected.to contain_package('letsencrypt').with_ensure('installed')
+        end
+
+        describe 'with package_ensure => 0.3.0-1.el7' do
+          let(:additional_params) { { install_method: 'package', package_ensure: '0.3.0-1.el7' } }
+
+          it { is_expected.to compile }
+          it { is_expected.to contain_package('letsencrypt').with_ensure('0.3.0-1.el7') }
+        end
+
+        case facts[:osfamily]
+        when 'RedHat'
+          describe 'with configure_epel => true' do
+            let(:additional_params) { { install_method: 'package', configure_epel: true } }
+
+            it { is_expected.to compile }
+
+            it 'contains the correct resources' do
+              is_expected.to contain_class('epel')
+              is_expected.to contain_package('letsencrypt').that_requires('Class[epel]')
+            end
+          end
+        end
       end
-    end
 
-    describe 'with package_ensure => 0.3.0-1.el7' do
-      let(:additional_params) { { install_method: 'package', package_ensure: '0.3.0-1.el7' } }
+      describe 'with install_method => vcs' do
+        let(:additional_params) { { install_method: 'vcs' } }
 
-      it { is_expected.to compile }
-      it { is_expected.to contain_package('letsencrypt').with_ensure('0.3.0-1.el7') }
-    end
-  end
+        it { is_expected.to compile }
 
-  describe 'with install_method => vcs' do
-    let(:additional_params) { { install_method: 'vcs' } }
+        it 'contains the correct resources' do
+          is_expected.to contain_vcsrepo('/opt/letsencrypt').with(source: 'https://github.com/certbot/certbot.git',
+                                                                  revision: 'v0.9.3')
+          is_expected.to contain_package('python')
+          is_expected.to contain_package('git')
 
-    it { is_expected.to compile }
+          is_expected.not_to contain_package('letsencrypt')
+        end
 
-    it 'contains the correct resources' do
-      is_expected.to contain_vcsrepo('/opt/letsencrypt').with(source: 'https://github.com/certbot/certbot.git',
-                                                              revision: 'v0.9.3')
-      is_expected.to contain_package('python')
-      is_expected.to contain_package('git')
+        describe 'with custom path' do
+          let(:additional_params) { { install_method: 'vcs', path: '/usr/lib/letsencrypt' } }
 
-      is_expected.not_to contain_package('letsencrypt')
-    end
+          it { is_expected.to contain_vcsrepo('/usr/lib/letsencrypt') }
+        end
 
-    describe 'with custom path' do
-      let(:additional_params) { { install_method: 'vcs', path: '/usr/lib/letsencrypt' } }
+        describe 'with custom repo' do
+          let(:additional_params) { { install_method: 'vcs', repo: 'git://foo.com/letsencrypt.git' } }
 
-      it { is_expected.to contain_vcsrepo('/usr/lib/letsencrypt') }
-    end
+          it { is_expected.to contain_vcsrepo('/opt/letsencrypt').with_source('git://foo.com/letsencrypt.git') }
+        end
 
-    describe 'with custom repo' do
-      let(:additional_params) { { install_method: 'vcs', repo: 'git://foo.com/letsencrypt.git' } }
+        describe 'with custom version' do
+          let(:additional_params) { { install_method: 'vcs', version: 'foo' } }
 
-      it { is_expected.to contain_vcsrepo('/opt/letsencrypt').with_source('git://foo.com/letsencrypt.git') }
-    end
+          it { is_expected.to contain_vcsrepo('/opt/letsencrypt').with_revision('foo') }
+        end
 
-    describe 'with custom version' do
-      let(:additional_params) { { install_method: 'vcs', version: 'foo' } }
+        describe 'with manage_dependencies set to false' do
+          let(:additional_params) { { install_method: 'vcs', manage_dependencies: false } }
 
-      it { is_expected.to contain_vcsrepo('/opt/letsencrypt').with_revision('foo') }
-    end
-
-    describe 'with manage_dependencies set to false' do
-      let(:additional_params) { { install_method: 'vcs', manage_dependencies: false } }
-
-      it 'does not contain the dependencies' do
-        is_expected.not_to contain_package('git')
-        is_expected.not_to contain_package('python')
+          it 'does not contain the dependencies' do
+            is_expected.not_to contain_package('git')
+            is_expected.not_to contain_package('python')
+          end
+        end
       end
     end
   end
