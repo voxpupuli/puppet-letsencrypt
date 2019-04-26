ce7149bcf0611f3c0f0c7f4f90ddb38ee711bef9
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2019-04-26 15:58:14 -0400

    refac(condition) use normal unless
    Favor a normal unless-statement

--- lib/facter/letsencrypt_directory.rb
+++ lib/facter/letsencrypt_directory.rb
@@ -8,15 +8,15 @@ Facter.add(:letsencrypt_directory) do
     # locate the certificate repository
     livedir = ['/etc/letsencrypt/live', '/etc/certbot/live'].map { |path| Pathname.new path }.find(&:directory?)
 
-    Pathname.new(livedir).children.select(&:directory?).each do |path|
-      pem = File.join(path, 'cert.pem')
-      cert = OpenSSL::X509::Certificate.new(File.new(pem).read)
-      san = cert.extensions.find { |e| e.oid == 'subjectAltName' }
-      names = san.value.split(',').map { |entry| entry.split(':')[1] }
-      names.each do |n|
-        certs[n] = path.to_s
+    unless livedir.nil
+      Pathname.new(livedir).children.select(&:directory?).each do |path|
+        pem = File.join(path, 'cert.pem')
+        cert = OpenSSL::X509::Certificate.new(File.new(pem).read)
+        san = cert.extensions.find { |e| e.oid == 'subjectAltName' }
+        names = san.value.split(',').map { |entry| entry.split(':')[1] }
+        names.each do |n|
+          certs[n] = path.to_s
+        end
       end
-    end unless livedir.nil?
-    certs
   end
 end
