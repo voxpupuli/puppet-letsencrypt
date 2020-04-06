48343324d7bea7eb3cf99ca0715b8b80e8bc38a6
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-06 23:11:39 +0200

    Use voxpupuli-acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,23 +1,11 @@
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_module
-install_module_dependencies
-
-RSpec.configure do |c|
-  # Configure all nodes in nodeset
-  c.before :suite do
-    hosts.each do |host|
-      # docker image does not provide cron in all cases
-      case fact('os.family')
-      when 'Debian'
-        host.install_package('cron')
-      when 'RedHat'
-        host.install_package('crontabs')
-      end
-    end
+configure_beaker do |host|
+  # docker image does not provide cron in all cases
+  case fact_on(host, 'os.family')
+  when 'Debian'
+    host.install_package('cron')
+  when 'RedHat'
+    host.install_package('crontabs')
   end
 end
