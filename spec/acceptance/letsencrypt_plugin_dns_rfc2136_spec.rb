6c81f3d9351e9d9a2b3ea90503d38809a0d52e1b
Nod0n <me@nodon.io>
2021-12-14 13:47:17 +0100

    drop obsolete check
    Debian 9 and Ubuntu 16.04 are not supported by this plugin, so therefore
    this check is obsolete.

--- spec/acceptance/letsencrypt_plugin_dns_rfc2136_spec.rb
+++ spec/acceptance/letsencrypt_plugin_dns_rfc2136_spec.rb
@@ -1,16 +1,6 @@
 require 'spec_helper_acceptance'
 
 describe 'letsencrypt::plugin::dns_rfc2136' do
-  supported = case fact('os.family')
-              when 'Debian'
-                # Debian 9 has it in backports, Ubuntu started shipping in Bionic
-                fact('os.release.major') != '9' && fact('os.release.major') != '16.04'
-              when 'RedHat'
-                true
-              else
-                false
-              end
-
   context 'with defaults values' do
     pp = <<-PUPPET
       class { 'letsencrypt' :
@@ -26,24 +16,18 @@ describe 'letsencrypt::plugin::dns_rfc2136' do
       }
     PUPPET
 
-    if supported
-      it 'installs letsencrypt and dns rfc2136 plugin without error' do
-        apply_manifest(pp, catch_failures: true)
-      end
-      it 'installs letsencrypt and dns rfc2136 idempotently' do
-        apply_manifest(pp, catch_changes: true)
-      end
+    it 'installs letsencrypt and dns rfc2136 plugin without error' do
+      apply_manifest(pp, catch_failures: true)
+    end
+    it 'installs letsencrypt and dns rfc2136 idempotently' do
+      apply_manifest(pp, catch_changes: true)
+    end
 
-      describe file('/etc/letsencrypt/dns-rfc2136.ini') do
-        it { is_expected.to be_file }
-        it { is_expected.to be_owned_by 'root' }
-        it { is_expected.to be_grouped_into 'root' }
-        it { is_expected.to be_mode 400 }
-      end
-    else
-      it 'fails to install' do
-        apply_manifest(pp, expect_failures: true)
-      end
+    describe file('/etc/letsencrypt/dns-rfc2136.ini') do
+      it { is_expected.to be_file }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+      it { is_expected.to be_mode 400 }
     end
   end
 end
