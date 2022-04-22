2f9cbc33b39a87f04508fa558bf1fb80ef6bb69a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-04-22 15:59:00 +0200

    Reduce acceptance test duplication
    This uses Hiera in the acceptance tests to reduce duplication in the
    manifests. To reduce the spec duplications it uses a shared example
    provided by voxpupuli-acceptance.

--- spec/acceptance/letsencrypt_plugin_dns_route53_spec.rb
+++ spec/acceptance/letsencrypt_plugin_dns_route53_spec.rb
@@ -3,24 +3,12 @@
 require 'spec_helper_acceptance'
 
 describe 'letsencrypt::plugin::dns_route53' do
-  context 'with defaults values' do
-    pp = <<-PUPPET
-      class { 'letsencrypt' :
-        email  => 'letsregister@example.com',
-        config => {
-          'server' => 'https://acme-staging-v02.api.letsencrypt.org/directory',
-        },
-      }
-      class { 'letsencrypt::plugin::dns_route53':
-      }
-    PUPPET
-
-    it 'installs letsencrypt and dns route53 plugin without error' do
-      apply_manifest(pp, catch_failures: true)
-    end
-
-    it 'installs letsencrypt and dns route53 idempotently' do
-      apply_manifest(pp, catch_changes: true)
+  it_behaves_like 'an idempotent resource' do
+    let(:manifest) do
+      <<-PUPPET
+      include letsencrypt
+      include letsencrypt::plugin::dns_route53
+      PUPPET
     end
   end
 end
--- spec/acceptance/letsencrypt_plugin_nginx_spec.rb
+++ spec/acceptance/letsencrypt_plugin_nginx_spec.rb
@@ -3,24 +3,12 @@
 require 'spec_helper_acceptance'
 
 describe 'letsencrypt::plugin::nginx' do
-  context 'with default values' do
-    pp = <<-PUPPET
-      class { 'letsencrypt' :
-        email  => 'letsregister@example.com',
-        config => {
-          'server' => 'https://acme-staging.api.letsencrypt.org/directory',
-        },
-      }
-      class { 'letsencrypt::plugin::nginx':
-      }
-    PUPPET
-
-    it 'installs letsencrypt and nginx plugin without error' do
-      apply_manifest(pp, catch_failures: true)
-    end
-
-    it 'installs letsencrypt and nginx idempotently' do
-      apply_manifest(pp, catch_changes: true)
+  it_behaves_like 'an idempotent resource' do
+    let(:manifest) do
+      <<-PUPPET
+      include letsencrypt
+      include letsencrypt::plugin::nginx
+      PUPPET
     end
   end
 end
