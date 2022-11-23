a513b572303a24e2d74cf8fe34b32ae6a987ef4f
Cédric De Wilde <c.dewilde@gmail.com>
2022-11-23 13:20:44 +0100

    Fix tests

--- spec/classes/plugin/dns_gandi_spec.rb
+++ spec/classes/plugin/dns_gandi_spec.rb
@@ -16,26 +16,21 @@ describe 'letsencrypt::plugin::dns_gandi' do
         }
         PUPPET
       end
+      let(:package_name) do
+        'python3-certbot-dns-gandi'
+      end
 
       context 'with required parameters' do
         it do
-          if package_name.nil?
-            is_expected.not_to compile
-          else
             is_expected.to compile.with_all_deps
-          end
         end
 
         describe 'with manage_package => true' do
           let(:params) { super().merge(manage_package: true) }
 
           it do
-            if package_name.nil?
-              is_expected.to compile.and_raise_error(%r{expects a value for parameter 'package_name'})
-            else
-              is_expected.to contain_class('letsencrypt::plugin::dns_gandi').with_package_name(package_name)
-              is_expected.to contain_package(package_name).with_ensure('installed')
-            end
+            is_expected.to contain_class('letsencrypt::plugin::dns_gandi').with_package_name(package_name)
+            is_expected.to contain_package(package_name).with_ensure('installed')
           end
         end
 
