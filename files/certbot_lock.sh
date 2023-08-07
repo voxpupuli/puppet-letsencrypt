f7289f95926dfa2702793b56f27f760322f38288
Tomas Barton <barton.tomas@gmail.com>
2023-08-07 15:19:09 +0200

    Shell and lockf compatibility

--- files/certbot_lock.sh
+++ files/certbot_lock.sh
@@ -1,14 +1,25 @@
+#!/bin/sh
 # Managed by Puppet
 LOCKFILE="/var/lock/certbot"
-LOCKFD=99
+LOCKFD=9
 # private
-function _lock()          { flock "$@" $LOCKFD; }
-function _release_lock()  { _lock -u; _lock -xn && rm -f $LOCKFILE; }
-function _prepare_lock()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _release_lock EXIT; }
+_lock() {
+  local lock_binary=$(which flock)
+  if [ -z "${lock_binary}" ]; then
+    lock_binary=$(which lockf)
+    if [ -z "${lock_binary}" ]; then
+      echo "flock/lockf binary wasn't found"
+      exit 1
+    fi
+  fi
+  ${lock_binary} "$@" $LOCKFD;
+}
+_release_lock()  { _lock -u; _lock -xn && rm -f $LOCKFILE; }
+_prepare_lock()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _release_lock EXIT; }
 
 # on start
 _prepare_lock
 
 # public
-function exlock()         { _lock -x --timeout 30; }   # obtain an exclusive lock
-function unlock()         { _lock -u; }                # drop a lock
+exlock()         { _lock -x --timeout 30; }   # obtain an exclusive lock
+unlock()         { _lock -u; }                # drop a lock
