0f992f37a8b1eaf0e582dabe8e42dd3750d298f2
Elliot Schleglemilch <elliot@schlegelmilch.org>
2024-07-13 03:10:25 +0000

    is this breaking static validations?

--- lib/puppet/functions/letsencrypt/letsencrypt_lookup.rb
+++ lib/puppet/functions/letsencrypt/letsencrypt_lookup.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-
+#
+# @return letsencrypt_directory for a particular domain.
 Puppet::Functions.create_function(:'letsencrypt::letsencrypt_lookup') do
   def letsencrypt_lookup(common_name)
     domain = common_name.split('.', 2)[1]
